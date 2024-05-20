66be32b44c775cfc002573275c3317959dd4ee75
LuisNav6 <luishnb4@gmail.com>
2024-05-20 13:36:51 -0600

    feature: add panel, create form, update form, and improve forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { SignUpComponent } from './features/sign-up/sign-up.component';
+import { ProductsComponent } from './features/products/products.component';
+import { NewProductComponent } from './shared/new-product/new-product.component';
+import { UpdateProductComponent } from './shared/update-product/update-product.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignUpComponent }
+  { path: 'signup', component: SignUpComponent },
+  { path: 'products', component: ProductsComponent},
+  { path: 'new-product', component: NewProductComponent},
+  { path: 'update-product', component: UpdateProductComponent }
 ];
 
 @NgModule({
---
+++ src/app/features/products/products.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+  productos = [
+    {nombre: 'Producto 1', precio: 100, descripcion: 'Descripción del producto 1', photo: 'hola.png'},
+    {nombre: 'Producto 2', precio: 200, descripcion: 'Descripción del producto 2', photo: 'hola.png'},
+    {nombre: 'Producto 2', precio: 200, descripcion: 'Descripción del producto 2', photo: 'hola.png'}
+  ];
+
+  constructor(private router: Router) { }
+
+  crearProducto() {
+    this.router.navigate(['/new-product']);
+  }
+  
+  actualizarProducto() {
+    this.router.navigate(['/update-product']);
+  }
+}
