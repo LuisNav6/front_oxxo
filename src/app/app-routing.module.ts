b812e2cb50eda64d6d5e8d924f1e6e4f59f36596
ClaudiaSando <al248156@edu.uaa.mx>
2024-05-23 12:40:33 -0600

    feature: sidebar, footer y añadí ngif a los botones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { SignUpComponent } from './features/sign-up/sign-up.component';
+import { HomeComponent } from './features/home/home.component';
+import { CarritoComponent } from './features/carrito/carrito.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignUpComponent }
+  { path: 'signup', component: SignUpComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'carrito', component: CarritoComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './features/login/login.component';
 import { SignUpComponent } from './features/sign-up/sign-up.component';
+import { ProductsComponent } from './features/products/products.component';
+import { HomeComponent } from './features/home/home.component';
+import { InventoryComponent } from './features/inventory/inventory.component';
+import { CarritoComponent } from './features/carrito/carrito.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BarComponent } from './shared/bar/bar.component';
+import { BranchOfficesComponent } from './features/branch-offices/branch-offices.component';
+import { ReportsComponent } from './features/reports/reports.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    ProductsComponent,
+    HomeComponent,
+    InventoryComponent,
+    CarritoComponent,
+    FooterComponent,
+    BarComponent,
+    BranchOfficesComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule,
